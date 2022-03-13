96f25540462d6c91d9632a8aed6dbfc59dc2805a
Voi Be Nho <voibenho@Vois-MacBook-Pro.local>
2022-03-14 00:55:13 +0700

    Add config extname for template engine

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
 import morgan from 'morgan';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import { engine } from 'express-handlebars';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 app.use(morgan('combined'));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', engine({extname: '.hbs'}));
+
+app.set('view engine', '.hbs');
 app.set('views',path.join(__dirname,'./resources/views'));
 
 app.get('/', (req, res) => {
-    console.log(path.join(__dirname,'/resources/views'));
     res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
 })
 
 app.listen(port, () => {
